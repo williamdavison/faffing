8036fa7041e54f577b81476bef9611a4319b9b1d
Qasim Asghar <qasimasghar@Admins-MacBook-Pro-2.local>
2016-05-26 16:22:32 +0100

    Amended to properly get JSON and pass to TableViewController.

--- Initialiser/Initialiser/ViewController.swift
+++ Initialiser/Initialiser/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var films: [[String: AnyObject]] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Grab our testing JSON file
+        let url = NSBundle.mainBundle().URLForResource("testing", withExtension: "json")
+        let data = NSData(contentsOfURL: url!)
+        
+        // Parse the JSON file into an object.
+        do {
+            let object = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+        
+            // Store JSON data to class variable of films
+            films = object["films"] as! [[String: AnyObject]]
+        } catch {
+            
+        }
+        
+        print(films)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,52 +36,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    // Grab our testing JSON file
-    let url = NSBundle.mainBundle().URLForResource("testing", withExtension: "json")
-    let data = NSData(contentsOfURL: url!)
-    
-    // Parse the JSON file into an object.
-    
-    do {
-    
-    let object = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
-    
-    if let films = object["films"] as? [[String: AnyObject]] {
-    
-				for film in films {
-    
-				if let name = film["title"] as? String {
-    
-				let fullName = name.componentsSeparatedByString(" ")
-    
-				var clue = "";
-    
-    for names in fullName {
-    
-    let initial = String(names.characters.prefix(1));
-    
-    clue += initial;
-    clue += " ";
-    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let tableViewController = segue.destinationViewController as? TableViewController
+        
+        // Pass films to TableViewController
+        tableViewController?.films = films
     }
-    
-				var output = "";
-    
-				output = name + " intials are: " + clue;
-    
-				print(output);
-    
-				}
-    
-    }
-    
-    }
-    
-    } catch {
-    
-    // Handle Error
-    
-    }
-
 }
-
